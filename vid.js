0c330f67a68fba130260ec7a56d1dee6c5e736c2
Capta1nHamster <100691740+SeleverViktor@users.noreply.github.com>
2024-12-05 13:21:52 +0100

    Add files via upload

---
+++ vid.js
@@ -0,0 +1,16 @@
+//wrapper function
+//functiom{ (__dirname,__filename,module,require,exports)
+
+
+
+
+
+
+
+
+onsole.log("__dirname");
+onsole.log("__filename");
+onsole.log("require");
+onsole.log("module");
+
+//}
