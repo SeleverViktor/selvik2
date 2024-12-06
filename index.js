cec8ff707b43cbede020bcadb01e74e5d9fe12cc
Capta1nHamster <100691740+SeleverViktor@users.noreply.github.com>
2024-12-06 11:02:02 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,14 @@
+console.log("ok");
+console.log(__dirname);
+console.log(__filename);
+const add = require('./calculator.js');
+console.log(add(2, 3));
+require('http');
+ 
+
+const server =http.createServer(function(req,res)
+{
+    res.writeHead(200);
+    res.end('Hello World!');
+});
+server.listen(8080);
