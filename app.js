f8128c6b7f93ce032d5b6d2a3ccb39b1fa9a764f
Capta1nHamster <100691740+SeleverViktor@users.noreply.github.com>
2024-12-09 12:23:45 +0100

    Add files via upload

--- app.js
+++ app.js
@@ -1,1 +1,19 @@
-console.log("szia");s
+
+const btn =document.createElement('button')
+btn.onclick=function(){
+    alert("ez egy natív kód");
+}
+btn.innerHTML="Nativ"
+document.getElementById("nativ-button-container").appendChild(btn);
+ 
+const gomb = React.createElement("button",
+{
+    onclick:function()
+    {
+        alert("ez egy natív kód")
+    },
+},
+"React gomb"
+)
+ReactDOM.render(gomb,documen.getElementById("react-button-container"));
+//helyi menü tartozik hozzá
